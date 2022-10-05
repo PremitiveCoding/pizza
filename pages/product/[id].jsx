3fc0967ee86ad67c1becee8a5d740d3a9bfc0377
PremitiveCoding <58826270+PremitiveCoding@users.noreply.github.com>
2022-10-05 22:01:43 +0100

    geek

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -97,7 +97,7 @@ const Product = ({ pizza }) => {
 
 export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(
-    `http://localhost:3000/api/products/${params.id}`
+    `https://pizzaking.vercel.app/api/products/${params.id}`
   );
   return {
     props: {
